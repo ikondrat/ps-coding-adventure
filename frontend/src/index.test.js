c61c4cb9a71145ec3199b600f327aec9e594fb0a
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2024-11-10 09:17:45 +0100

    feat: setup vitest for testing

---
+++ frontend/src/index.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+// import { add } from '../lib/index.js';
+
+describe('sum test', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(1).toBe(1)
+  })
+})
