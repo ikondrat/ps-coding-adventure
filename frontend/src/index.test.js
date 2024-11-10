6bc29b76e9acd2517d1f9be2d191da5be2a06bf1
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2024-11-10 09:58:36 +0100

    feat: introduce vitest with example

--- frontend/src/index.test.js
+++ frontend/src/index.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest'
 // import { add } from '../lib/index.js';
 
 describe('sum test', () => {
--- frontend/vitest.config.mjs
+++ frontend/vitest.config.mjs
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
+  esbuild: {
+    // Transpile all files with ESBuild to remove comments from code coverage.
+    // Required for `test.coverage.ignoreEmptyLines` to work:
+    include: ['**/*.js', '**/*.jsx', '**/*.mjs', '**/*.ts', '**/*.tsx']
+  },
   test: {
     globals: true,
-    environment: 'jsdom'
+    environment: 'jsdom',
+    setupFiles: ['src/setupTest.ts'],
+    coverage: {
+      provider: 'v8',
+      ignoreEmptyLines: true
+    }
   }
 })
