fb3cfdbec9a6a33994f5429a1aee62ba13de8050
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2024-11-10 11:03:21 +0100

    load variable from env

--- backend/src/database.py
+++ backend/src/database.py
@@ -3,10 +3,14 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 import os
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 Base = declarative_base()
 
-DATABASE_URL = os.getenv("TEST_DATABASE_URL") if os.getenv("TESTING") == "True" else os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(
